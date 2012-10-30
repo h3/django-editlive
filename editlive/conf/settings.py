dc73a8ea12c225b01c4a2ef9dd7274d30a6cd340
Your Name <haineault@gmail.com>
2012-10-30 08:54:39 -0400

    Updated settings

--- editlive/conf/settings.py
+++ editlive/conf/settings.py
@@ -1,30 +1,3 @@
-"""
-TODO: move to a register system instead, which will allow more complex adaptor schemes
-
-ex:
-
-import editlive
-
-class TextAdaptorOption(AdaptorOption):
-    auto_fields = ['django.db.models.CharField']
-    is_active(self, field):
-        return not getattr(field, 'choices')
-editlive.register(TextAdaptorOption)
-
-
-class TextChoicesAdaptorOption(TextAdaptorOption):
-    get_auto_field(self):
-        fields = self.auto_fields
-        fields.append('custom.models.CharField')
-        return fields
-
-    is_active(self, field):
-        return getattr(field, 'choices')
-editlive.register(TextChoicesAdaptorOption)
-
-
-"""
-
 EDITLIVE_DEFAULT_ADAPTORS = {
     # Fields
     'char':     'editlive.adaptors.CharAdaptor',
@@ -35,7 +8,6 @@ EDITLIVE_DEFAULT_ADAPTORS = {
     'fk':       'editlive.adaptors.ForeignKeyAdaptor',
     'choices':  'editlive.adaptors.ChoicesAdaptor',
     'm2m':      'editlive.adaptors.ManyToManyAdaptor',
-   #'textarea': 'editlive.adaptors.TextAreaAdaptor',
    #'file':     'editlive.adaptors.FileAdaptor',
    #'image':    'editlive.adaptors.ImageAdaptor',
 
