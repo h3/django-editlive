001c594629b960261a2b6cbc245ec32c712e7ee2
h3 <max@motion-m.ca>
2012-12-31 01:20:16 -0500

    Lettuce tests running with selenium \!

--- editlive/test_app/features/charfield.feature
+++ editlive/test_app/features/charfield.feature
@@ -1,12 +1,11 @@
-Feature: charField basic tests
+# Editlive charField tests
+
+Feature: charField
+    Functional tests for the editlive charField widget
 
     Scenario: Initial state
         Given I access the url "/editlive/test/charfield/"
         Then I see a field named "char_test" with the id "id_char_test"
-        Then I see a editlive of type "charField" with the field id "id_char_test"
-
-
-    Scenario: Placeholder test
-        Given I access the url "/editlive/test/charfield/"
-        Then I click on the placeholder next to "id_char_test" and see the input field appear
-
+        Then I see a editlive of type "charField" for the field id "id_char_test"
+        Then I see the HTML node with the id "id_char_test" is hidden
+        Then I see the placeholder for the id "id_char_test" is visible
--- editlive/test_app/features/fields.py
+++ editlive/test_app/features/fields.py
@@ -1,32 +1,78 @@
 from lettuce import *
 from lxml import html
-from django.test.client import Client
+#from django.test.client import Client
 from nose.tools import assert_equals
+from lettuce_webdriver.util import assert_false
+from lettuce_webdriver.util import AssertContextManager
+from selenium import webdriver
+
 
 @before.all
 def set_browser():
-    world.browser = Client()
+    world.browser = webdriver.Firefox()
+#   world.browser = Client()
+
+
+def S(selector):
+    return world.browser.find_element_by_css_selector(selector)
+
 
 @step(r'I access the url "(.*)"')
 def access_url(step, url):
-    response = world.browser.get(url)
-    world.dom = html.fromstring(response.content)
+    response = world.browser.get('http://localhost:8000'+ url)
+#   world.dom = html.fromstring(response.content)
+
 
 @step(r'I see a field named "(.*)" with the id "(.*)"')
 def see_field(step, fieldname, field_id):
-    field = world.dom.cssselect('#'+ field_id)
-    assert len(field) > 0 and field[0].attrib['name'] == fieldname 
+    field = S('#'+field_id)
+    assert field.tag_name == 'input'
+    assert field.get_attribute('type') == 'text'
+    assert field.get_attribute('maxlength') == '250'
+    assert field.get_attribute('name') == fieldname
 
 
-@step(r'Then I see a editlive of type "(.*)" with the field id "(.*)"')
+@step(r'I see a editlive of type "(.*)" for the field id "(.*)"')
 def see_editlive(step, fieldtype, field_id):
-    editlive = world.dom.cssselect('editlive[data-field-id="'+field_id+'"]')
-    assert len(editlive) > 0 and editlive[0].attrib['data-type'] == fieldtype
+    editlive = S('editlive[data-field-id="'+field_id+'"]')
+    assert editlive.get_attribute('data-type') == fieldtype
+ 
+
+@step(r'I see the HTML node with the id "(.*)" is (hidden|visible)')
+def see_html_node_visible_or_hidden(step, node_id, state):
+    node = S('#'+node_id)
+    if state == 'hidden':
+        assert node.is_displayed() is False
+    else:
+        assert node.is_displayed()
+ 
+
+@step(r'I see the placeholder for the id "(.*)" is (hidden|visible)')
+def see_placeholder_visible_or_hidden(step, node_id, state):
+    span = S('#'+node_id+' + span')
+    assert 'editlive' in span.get_attribute('class').split(' ')
+    assert span.is_displayed()
+ 
+
+#@step(r'I click on the placeholder next to "(.*)" and see the input field appear')
+#def see_placeholder(step, field_id):
+#    field = world.dom.cssselect('#'+field_id)
+#    assert len(field) > 0
+ 
+ 
 
 
-@step(r'I click on the placeholder next to "(.*)" and see the input field appear')
-def see_placeholder(step, field_id):
-    field = world.dom.cssselect('#'+field_id)
-    import IPython
-    IPython.embed()
-    assert len(editlive) > 0 and editlive[0].attrib['data-type'] == fieldtype
+#@step('I fill in field with class "(.*?)" with "(.*?)"')
+#def fill_in_textfield_by_class(step, field_name, value):
+#    with AssertContextManager(step):
+#        text_field = find_field_by_class(world.browser, field_name)
+#        text_field.clear()
+#        text_field.send_keys(value)
+
+
+
+
+#   Scenario: Placeholder test
+#       Given I access the url "/editlive/test/charfield/"
+#       Then I click on the placeholder next to "id_char_test" and see the input field appear
+
