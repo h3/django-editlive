309509a2592a034dd45dfcaf3471db62a6c80f37
h3 <max@motion-m.ca>
2013-01-03 12:28:32 -0500

    Removed old test_app

--- editlive/test_app_old/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.contrib import admin
-from editlive.test_app.models import EditliveBaseFieldsTest
-
-
-class EditliveBaseFieldsTestAdmin(admin.ModelAdmin):
-    list_display = ('char_test', 'text_test')
-admin.site.register(EditliveBaseFieldsTest, EditliveBaseFieldsTestAdmin)
--- editlive/test_app_old/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls.defaults import *
-from editlive.test_app.views import *
-
-urlpatterns=patterns('',
-    url(r'^test/(?P<name>\w+)/$', TestView.as_view(), name='editlive-test'),
-)
