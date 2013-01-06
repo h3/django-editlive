e76ce8133eaa511dc5484cfadd9f89a2d2c655ea
h3 <max@motion-m.ca>
2013-01-06 14:33:12 -0500

    Fixed bug with DateAdaptor

--- editlive/adaptors/date.py
+++ editlive/adaptors/date.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from editlive.adaptors.base import BaseAdaptor
 
 
@@ -8,19 +9,14 @@ class DateAdaptor(BaseAdaptor):
         super(DateAdaptor, self).__init__(*args, **kwargs)
         field = self.form.fields.get(self.field_name)
         if field:
-           #import IPython
-           #IPython.embed()
             self.attributes.update({'data-format': '%s' % field.widget.format})
-
-
         if self.form_field:
             self.attributes.update({'data-type': 'dateField'})
 
     def render_value(self, value=None):
-       #import IPython
-       #IPython.embed()
-        if self.template_filters and not any(item.startswith('date:')\
-                for item in self.template_filters):
+        if self.template_filters is None:
+            self.template_filters = []
+        if not any(i.startswith('date:') for i in self.template_filters):
             self.template_filters.append(u"date:'%s'" % settings.DATE_FORMAT)
         return unicode(super(DateAdaptor, self).render_value(value=value))
 
@@ -34,11 +30,11 @@ class DateTimeAdaptor(BaseAdaptor):
             self.attributes.update({'data-type': 'datetimeField'})
 
     def render_value(self, value=None):
-        if self.template_filters and not any(item.startswith('date:')\
-                for item in self.template_filters):
-            self.template_filters.append(\
-                    u"date:'%s'" % settings.DATETIME_FORMAT)
-        return super(DateTimeAdaptor, self).render_value(value=value)
+        if self.template_filters is None:
+            self.template_filters = []
+        if not any(i.startswith('date:') for i in self.template_filters):
+            self.template_filters.append(u"date:'%s'" % settings.DATETIME_FORMAT)
+        return unicode(super(DateTimeAdaptor, self).render_value(value=value))
 
 
 class TimeAdaptor(BaseAdaptor):
