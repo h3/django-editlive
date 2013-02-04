80c80993615ca8480c8b745187fa3d491dca689b
h3 <max@motion-m.ca>
2013-02-04 01:25:45 -0500

    Added test for multiple editlive instance, added test index page

--- example_project/test_app/urls.py
+++ example_project/test_app/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 
-from test_app.views import TestView
+from test_app.views import TestView, TestSoupView
 
 urlpatterns=patterns('',
+    url(r'^soup/$', TestSoupView.as_view(), name='editlive-test-soup'),
     url(r'^(?P<field>\w+)/$', TestView.as_view(), name='editlive-test'),
 )
